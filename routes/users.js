c2ac9b0d6c6b9080aad52e9b56ab8bcc782eaeb1
David Leeper <David.Leeper@davita.com>
2017-10-12 13:08:24 -0600

    Moved all config files to a single config file. Removed users.js

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var jsonResponseConfig = require('../json-response-config.json');
 
 /* GET users listing. */
 router.get('*', function(req, res, next) {
-    alert("req.path: " + req.path);
-    alert("jsonResponseConfig[req.path]: " + jsonResponseConfig[req.path]);
-  if (jsonResponseConfig[req.path]) {
-    var responseFileContents = require(jsonResponseConfig[req.path]);
-    res.send(JSON.stringify(responseFileContents));
-  }
+  res.send('respond with a resource');
 });
 
 module.exports = router;
