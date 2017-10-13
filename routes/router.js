ae000f5b10e6e06ab6f6dd441732d90d62a6cedb
David Leeper <David.Leeper@davita.com>
2017-10-13 16:14:11 -0600

    Added additional router testing

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@
 
 var util = require ( '../util/utilities.js' );
 var log = require ( '../util/logger-utilities.js' );
+var fs = require("fs");
 
 function Router ( ) { }
 
@@ -10,40 +11,7 @@ Router.startTime = null;
 Router.serverConfig = null;
 
 Router.init = function ( router ) {
-    router.get('*', function(req, res) {
-        if ( log.will( log.ALL )) log.all( "Router.init: Rounting " + req.path );
-        var mockResponseInfo = Router.getMockResponseInfo(req.path);
-
-        if (mockResponseInfo) {
-            Router.addHeaders(mockResponseInfo, res);
-            if ("JSON" == mockResponseInfo.fileType.toString().toUpperCase()) {
-                var jsonResponseFileContents = require(mockResponseInfo.responseFile);
-
-                res.send(JSON.stringify(jsonResponseFileContents));
-            } else if ("HBS" == mockResponseInfo.fileType.toString().toUpperCase()) {
-                res.render(mockResponseInfo.responseFile, mockResponseInfo.hbsData);
-            } else {
-                var textResponseFileContents = util.readFileSync(mockResponseInfo.responseFile, mockResponseInfo.encoding);
-
-                res.send(textResponseFileContents);
-            }
-            return;
-        }
-
-        var servicesInfo = Router.getServiceInfo(req.path);
-
-        if (servicesInfo) {
-            var servicePath = "../services/" + servicesInfo.serviceFile;
-            var serviceClass = require(servicePath);
-            var service = new serviceClass();
-
-            Router.addHeaders(servicesInfo, res);
-            service.do(req, res, Router, servicesInfo);
-            return;
-        }
-
-        Router.defaultResponse(res);
-    });
+    router.get('*', Router.route);
     return router;
 };
 
@@ -85,11 +53,54 @@ Router.getServiceInfo = function ( path ) {
         var responseRecord = Router.serverConfig.services[loop];
 
         if ((responseRecord.path == path)
-        && (responseRecord.serviceFile)) {
+            && (responseRecord.serviceFile)) {
             return responseRecord;
         }
     }
     return null;
+}
+
+Router.route = function ( req, res ) {
+    if ( log.will( log.ALL )) log.all( "Router.router: Rounting " + req.path );
+    var mockResponseInfo = Router.getMockResponseInfo(req.path);
+
+    if (mockResponseInfo) {
+        Router.addHeaders(mockResponseInfo, res);
+        if ("JSON" == mockResponseInfo.fileType.toString().toUpperCase()) {
+            if (!fs.existsSync(mockResponseInfo.responseFile)) {
+                res.render("not-found", null);
+                return;
+            };
+            var jsonResponseFileContents = util.readFileSync(mockResponseInfo.responseFile);
+
+            res.send(jsonResponseFileContents);
+        } else if ("HBS" == mockResponseInfo.fileType.toString().toUpperCase()) {
+            res.render(mockResponseInfo.responseFile, mockResponseInfo.hbsData);
+        } else {
+            if (!fs.existsSync(mockResponseInfo.responseFile)) {
+                res.render("not-found", null);
+                return;
+            };
+            var textResponseFileContents = util.readFileSync(mockResponseInfo.responseFile, mockResponseInfo.encoding);
+
+            res.send(textResponseFileContents);
+        }
+        return;
+    }
+
+    var servicesInfo = Router.getServiceInfo(req.path);
+
+    if (servicesInfo) {
+        var servicePath = "../services/" + servicesInfo.serviceFile;
+        var serviceClass = require(servicePath);
+        var service = new serviceClass();
+
+        Router.addHeaders(servicesInfo, res);
+        service.do(req, res, Router, servicesInfo);
+        return;
+    }
+
+    Router.defaultResponse(res);
 }
 
 module.exports = Router;
