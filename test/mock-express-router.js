b33a50214e399961cc4c6ff7c449d28d6289db05
David Leeper <David.Leeper@davita.com>
2017-10-13 14:51:29 -0600

    Added tests for Router

---
+++ test/mock-express-router.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/**
+ * @constructor
+ */
+function MockExpressRouter ( )
+{
+    this.path = null;
+    this.handler = null;
+}
+
+/**
+ * @param path {String} - The GET path to be handled by the router.
+ * @param handler {Function} - The function invoked when the GET path is referenced by the user.
+ */
+MockExpressRouter.prototype.get = function ( path, handler )
+{
+    this.path = path;
+    this.handler = handler;
+};
+
+module.exports = MockExpressRouter;
