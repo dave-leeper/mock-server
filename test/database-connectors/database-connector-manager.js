e9490bdd1ef6325914807fa7270d27c60d789f12
David Leeper <David.Leeper@davita.com>
2017-10-18 16:26:35 -0600

    Added database connectors

---
+++ test/database-connectors/database-connector-manager.js
@@ -0,0 +1,37 @@
+//@formatter:off
+'use strict';
+
+var chai = require( 'chai' ),
+    expect = chai.expect,
+    DatabaseConnectorManager = require('../../database-connectors/database-connector-manager.js');
+var config = {
+    databaseConnections : [
+        {
+            name: "elasticsearch",
+            description: "Elasticsearch service.",
+            databaseConnector: "elasticsearch.js",
+            config: {
+                host: "localhost:9200",
+                log: "trace"
+            }
+        }
+    ]
+
+};
+
+describe( 'As a developer, I need to manage database connections.', function()
+{
+    it ( 'It should create all requested database connections', ( ) => {
+        let dcm = new DatabaseConnectorManager();
+
+        expect(dcm).to.not.be.null;
+        dcm.init(config);
+        expect(dcm.config).to.be.equal(config);
+        expect(dcm.databaseConnectors.length).to.be.equal(1);
+
+        let elasticSearchDC = dcm.getConnector("elasticsearch");
+        expect(elasticSearchDC).to.not.be.null;
+    });
+});
+
+
