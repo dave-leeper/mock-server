04367a51e34185684b7527704b970201988afb68
David Leeper <David.Leeper@davita.com>
2017-10-19 16:14:06 -0600

    Added tests for the server.

--- database-connectors/elasticsearch.js
+++ database-connectors/elasticsearch.js
@@ -8,7 +8,24 @@ function ElasticSearchDatabaseConnector ( ) {
 }
 
 ElasticSearchDatabaseConnector.prototype.connect = function (config ) {
-    this.client = new elasticsearch.Client(config.config);
+    // Elasticsearch mangles configs, so copy it.
+    let configCopy = {};
+    let prop;
+
+    for (prop in config.config) {
+        configCopy[prop] = config.config[prop];
+    }
+    this.client = new elasticsearch.Client(configCopy);
+    this.config = config;
+    return this.client;
+};
+
+ElasticSearchDatabaseConnector.prototype.disconnect = function ( ) {
+    if (!this.client) {
+        return;
+    }
+    elasticsearch.Client(config.config).
+    this.client.
     this.config = config;
     return this.client;
 };
