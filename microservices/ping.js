268cf8f9e4d81194d5f81e6725f3ac077faabae0
David Leeper <David.Leeper@davita.com>
2017-10-20 12:25:46 -0600

    Added microservices tests. Replaced deleted ping service code.

---
+++ microservices/ping.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * @constructor
+ */
+function PingService ( )
+{
+}
+
+/**
+ * @param req {Object} - The request object.
+ * @param res {Object} - The response object.
+ * @param router - The router.
+ * @param serviceInfo - Service config info.
+ */
+PingService.prototype.do = function ( req, res, router, serviceInfo )
+{
+    return new Promise (( inResolve ) => {
+        if ((!serviceInfo) || (!serviceInfo.serviceData)) {
+            res.send(JSON.stringify({"response": "Server running."}));
+            inResolve && inResolve ( null, this );
+            return;
+        }
+
+        res.send(JSON.stringify(serviceInfo.serviceData));
+        inResolve && inResolve ( null, this );
+    });
+};
+
+module.exports = PingService;
