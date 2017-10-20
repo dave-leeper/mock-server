36c29e63d323caafead97398253f03dbf392bd5c
David Leeper <David.Leeper@davita.com>
2017-10-20 15:25:58 -0600

    Changed all database connection calls to return Promises. Added tests for elasticsearch.

--- database-connectors/elasticsearch.js
+++ database-connectors/elasticsearch.js
@@ -2,32 +2,53 @@
 
 var elasticsearch = require('elasticsearch');
 
-function ElasticSearchDatabaseConnector ( ) {
+function ElasticSearchDatabaseConnector ( name ) {
+    this.name = name;
     this.client = null;
     this.config = null;
 }
 
 ElasticSearchDatabaseConnector.prototype.connect = function (config ) {
-    // Elasticsearch mangles configs, so copy it.
-    let configCopy = {};
-    let prop;
+    return new Promise (( inResolve ) => {
+        // Elasticsearch mangles configs, so copy it.
+        let configCopy = {};
+        let prop;
 
-    for (prop in config.config) {
-        configCopy[prop] = config.config[prop];
-    }
-    this.client = new elasticsearch.Client(configCopy);
-    this.config = config;
-    return this.client;
+        for (prop in config.config) {
+            configCopy[prop] = config.config[prop];
+        }
+        this.client = new elasticsearch.Client(configCopy);
+        this.config = config;
+        inResolve && inResolve ( null, this.client );
+    });
+};
+
+ElasticSearchDatabaseConnector.prototype.ping = function (  )
+{
+    return new Promise (( inResolve ) => {
+        if (!this.client) {
+            inResolve && inResolve ( false );
+        } else {
+            this.client.ping({ requestTimeout: 30000, }, function( error ) {
+                if (error) {
+                    inResolve && inResolve ( false );
+                } else {
+                    inResolve && inResolve ( true );
+                }
+            });
+        }
+    });
 };
 
 ElasticSearchDatabaseConnector.prototype.disconnect = function ( ) {
-    if (!this.client) {
-        return;
-    }
-    elasticsearch.Client(config.config).
-    this.client.
-    this.config = config;
-    return this.client;
+    return new Promise (( inResolve ) => {
+        if (!this.client) {
+            inResolve && inResolve ( false );
+        } else {
+            this.client.close();
+            inResolve && inResolve ( true );
+        }
+    });
 };
 
 module.exports = ElasticSearchDatabaseConnector;
