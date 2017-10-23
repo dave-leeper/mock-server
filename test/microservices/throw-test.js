d321a0371fae4c808e2431f3968559ddfa6df74f
David Leeper <David.Leeper@davita.com>
2017-10-23 11:00:16 -0600

    Added test for throw microservice. Removed uneeded config options from tests. Fixed a bug that caused server to fail when no mocks where in config.

---
+++ test/microservices/throw-test.js
@@ -0,0 +1,47 @@
+//@formatter:off
+'use strict';
+
+var chai = require( 'chai' ),
+    expect = chai.expect,
+    Router = require('../../routes/router.js'),
+    Server = require('../../server.js'),
+    MockRequest = require('../mock-request.js'),
+    MockResponse = require('../mock-response.js'),
+    request = require('request');
+var config = {
+    "microservices": [
+        {
+            "path": "/ping",
+            "name": "Ping",
+            "description": "A basic ping service.",
+            "serviceFile": "ping.js",
+            "serviceData": { "name": "My Server", "version": "1.0" },
+            "headers": [ { "header": "MY_HEADER", "value": "MY_HEADER_VALUE" } ]
+        },
+        {
+            "path": "/throw",
+            "name": "Throw Exception",
+            "description": "A micro service that throws an exception. For testing purposes.",
+            "serviceFile": "throw.js"
+        }
+    ]
+};
+
+describe( 'As a developer, I need the server to continue running when exceptions are thrown.', function()
+{
+    it ( 'should continue running after an exception is thrown', ( done ) => {
+        let port = '1337';
+        let server = new Server();
+        let req = new MockRequest('/throw');
+        let pingResponse = '{"name":"My Server","version":"1.0"}';
+        let serverInitCallback = () => {
+            request('http://localhost:' + port + "/throw", { json: true }, (err, res, body) => {
+                request('http://localhost:' + port + "/ping", { json: true }, (err, res, body) => {
+                    expect(JSON.stringify(body)).to.be.equal(pingResponse);
+                    server.stop(() => {done(); });
+                });
+            });
+        };
+        server.init( port, config, serverInitCallback );
+     });
+});
