8fc027b2c200e4a91c8949bd853ebd26a3804c21
David Leeper <David.Leeper@davita.com>
2017-10-26 16:11:34 -0600

    Added file download

---
+++ microservices/download.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const path = require('path');
+let files = require ( '../util/file-utilities.js' );
+
+const FILE_PATH = path.join(__dirname , "/../files/");
+/**
+ * @constructor
+ */
+function DownloadService ( )
+{
+}
+
+/**
+ * @param req {Object} - The request object.
+ * @param res {Object} - The response object.
+ * @param router - The router.
+ * @param serviceInfo - Service config info.
+ */
+DownloadService.prototype.do = function ( req, res, router, serviceInfo )
+{
+    return new Promise (( inResolve ) => {
+        console.log("DownloadService");
+        const fileName = ((req.params.name)? req.params.name : "filename");
+        const filePath = path.join(FILE_PATH, fileName);
+
+        if ( !files.existsSync( filePath ) ) {
+            res.render("not-found", { title: fileName });
+            inResolve && inResolve ( null, this );
+        } else {
+            res.download(filePath);
+
+            inResolve && inResolve(null, this);
+        }
+        return;
+    });
+};
+
+module.exports = DownloadService;
