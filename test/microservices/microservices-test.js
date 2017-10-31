be156a2a28b7f9db562dc4e6c513376746967767
David Leeper <David.Leeper@davita.com>
2017-10-31 09:12:20 -0600

    Added stop test.

--- test/microservices/microservices-test.js
+++ test/microservices/microservices-test.js
@@ -5,6 +5,7 @@ var chai = require( 'chai' ),
     expect = chai.expect,
     Router = require('../../router.js'),
     MicroservicesMicroservice = require('../../microservices/microservices.js'),
+    MockRequest = require('../mock-request.js'),
     MockResponse = require('../mock-response.js');
 var config = {
     "microservices": [
@@ -25,17 +26,15 @@ var config = {
 
 describe( 'As a developer, I need need to obtain a list of microservices that are available.', function()
 {
-
     it ( 'should return a list of microservices available.', ( ) => {
         let microservicesMicroservice = new MicroservicesMicroservice();
+        let mockRequest = new MockRequest();
         let mockResponse = new MockResponse();
-        let mockRequest = {};
         let mockServiceInfo = {};
-        let expectedResponse = '[{"path":"/microservices","name":"Services List","description":"Provides a list of microservices registered with this server."},{"path":"/mocks","name":"Mock Services List","description":"Provides a list of mock microservices registered with this server."}]';
+        let expectedResponse = '[{"name":"Services List","path":"/microservices","description":"Provides a list of microservices registered with this server."},{"name":"Mock Services List","path":"/mocks","description":"Provides a list of mock microservices registered with this server."}]';
 
-        Router.server = {};
-        Router.server.serverConfig = config;
-        microservicesMicroservice.do(mockRequest, mockResponse, Router, mockServiceInfo);
+        mockRequest.app.locals.___extra.serverConfig = config;
+        microservicesMicroservice.do(mockRequest, mockResponse, mockServiceInfo);
         expect(mockResponse.sendString).to.be.equal(expectedResponse);
     });
 });
