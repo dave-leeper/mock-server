3b0896efde49653048610d47d3ce39afee5f6c09
David Leeper <David.Leeper@davita.com>
2017-10-31 09:11:37 -0600

    Added stop test.

--- microservices/stop.js
+++ microservices/stop.js
@@ -10,19 +10,19 @@ function StopService ( )
 /**
  * @param req {Object} - The request object.
  * @param res {Object} - The response object.
- * @param router - The router.
  * @param serviceInfo - Service config info.
  */
-StopService.prototype.do = function ( req, res, router, serviceInfo )
+StopService.prototype.do = function ( req, res, serviceInfo )
 {
     return new Promise (( inResolve ) => {
-        if ((!router) || (!router.server)) {
+        if ((!req.app) || (!req.app.locals) || (!req.app.locals.___extra) || (!req.app.locals.___extra.server)) {
             res.send(JSON.stringify({ "response": "Server not found." }));
             inResolve && inResolve ( null, this );
             return;
         }
 
-        router.server.stop( () => { inResolve && inResolve ( null, this ) } );
+        res.send(JSON.stringify({ "response": "Server stopping." }));
+        req.app.locals.___extra.server.stop( () => { inResolve && inResolve ( null, this ) } );
     });
 };
 
