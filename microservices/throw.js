a5de7677064cedd0832691e511a832c05df5658e
David Leeper <David.Leeper@davita.com>
2017-10-31 09:11:44 -0600

    Added stop test.

--- microservices/throw.js
+++ microservices/throw.js
@@ -10,10 +10,9 @@ function ThrowService ( )
 /**
  * @param req {Object} - The request object.
  * @param res {Object} - The response object.
- * @param router - The router.
  * @param serviceInfo - Service config info.
  */
-ThrowService.prototype.do = function ( req, res, router, serviceInfo )
+ThrowService.prototype.do = function ( req, res, serviceInfo )
 {
     throw Error("ERROR");
 };
