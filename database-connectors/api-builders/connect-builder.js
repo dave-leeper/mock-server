988dfcd3f610d47df004c68be5489b2cafb9d279
David Leeper <David.Leeper@davita.com>
2017-11-03 10:58:18 -0600

    Some refactoring of DB connection API.

--- database-connectors/api-builders/connect-builder.js
+++ database-connectors/api-builders/connect-builder.js
@@ -4,12 +4,15 @@ function ConnectBuilder ( routerClass, databaseConnectionInfo ) {
     let connectHandler = (req, res) => {
         routerClass.addHeaders(databaseConnectionInfo, res);
 
-        let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
-        if (!databaseConnectionManager) {
+        if ((!req)
+        || (!req.app)
+        || (!req.app.locals)
+        || (!req.app.locals.___extra)
+        || (!req.app.locals.___extra.databaseConnectionManager)) {
             res.render("error", {message: "No database connection manager.", error: {status: 500}});
             return;
         }
-
+        let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
         let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
         if (!databaseConnection) {
             res.render("error", {message: "No database connection.", error: {status: 500}});
@@ -17,7 +20,7 @@ function ConnectBuilder ( routerClass, databaseConnectionInfo ) {
         }
 
         databaseConnection.connect(databaseConnectionInfo).then(() => {
-            res.send("Connected " + databaseConnectionInfo.name + " established.");
+            res.send({databaseConnection: databaseConnectionInfo.name, operation: "connect", isConnected: true});
         });
     };
 
