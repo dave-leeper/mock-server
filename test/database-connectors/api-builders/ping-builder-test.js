68eab91a23859cdc7f4bbdc28b45fd905ed86655
David Leeper <David.Leeper@davita.com>
2017-11-06 09:44:41 -0700

    Minor sytax cleanups

--- test/database-connectors/api-builders/ping-builder-test.js
+++ test/database-connectors/api-builders/ping-builder-test.js
@@ -1,14 +1,14 @@
 //@formatter:off
 'use strict';
 
-var chai = require( 'chai' ),
+let chai = require( 'chai' ),
     expect = chai.expect,
     Router = require('../../../router.js'),
     mockRequest = require('../../mock-request.js'),
     mockResponse = require('../../mock-response.js'),
     pingHandlerBuilder = require('../../../database-connectors/api-builders/ping-builder.js'),
     DatabaseConnectorManager = require('../../../database-connectors/database-connector-manager.js');
-var config = {
+let config = {
     "databaseConnections" : [
         {
             "name": "elasticsearch",
