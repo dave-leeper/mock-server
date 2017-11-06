7c39dbabe04ddc35fc99551415ed216918cf7d42
David Leeper <David.Leeper@davita.com>
2017-11-06 09:16:32 -0700

    Minor sytax cleanups

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var Server = require('./server.js');
-var port = process.env.PORT || '3000';
-var serverConfig = require('./server-config.json');
-var server = new Server().init(port, serverConfig);
+let Server = require('./server.js');
+let port = process.env.PORT || '3000';
+let serverConfig = require('./server-config.json');
+let server = new Server().init(port, serverConfig);
 
 module.exports = server;
