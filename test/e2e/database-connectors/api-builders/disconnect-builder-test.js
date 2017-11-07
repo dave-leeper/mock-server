2b9504326ee1e739219471683ae19dd21d173b60
David Leeper <David.Leeper@davita.com>
2017-11-07 11:21:37 -0700

    Added testing for creating, dropping, and checking existence of tables.

---
+++ test/e2e/database-connectors/api-builders/disconnect-builder-test.js
@@ -0,0 +1,42 @@
+//@formatter:off
+'use strict';
+
+let chai = require( 'chai' ),
+    expect = chai.expect,
+    Router = require('../../../../router.js'),
+    mockRequest = require('../../../mock-request.js'),
+    mockResponse = require('../../../mock-response.js'),
+    disconnectionHandlerBuilder = require('../../../../database-connectors/api-builders/disconnect-builder.js'),
+    DatabaseConnectorManager = require('../../../../database-connectors/database-connector-manager.js');
+let config = {
+    "databaseConnections" : [
+        {
+            "name": "elasticsearch",
+            "description": "Elasticsearch service.",
+            "databaseConnector": "elasticsearch.js",
+            "generateConnectionAPI": true,
+            "config": {
+                "host": "localhost:9200",
+                "log": "trace"
+            }
+        }
+    ]
+};
+
+describe( 'As a developer, I need an API for database connections', function() {
+    it ( 'should build a handler for requests to disconnect from the database', ( ) => {
+        let connectionHandler = disconnectionHandlerBuilder( Router, config.databaseConnections[0] );
+        expect(connectionHandler).to.not.be.null;
+
+        let req = new mockRequest();
+        let res = new mockResponse();
+        connectionHandler( req, res );
+        expect(res.renderString).to.be.equal("error");
+        expect(JSON.stringify(res.renderObject)).to.be.equal(JSON.stringify({message: "No database connection manager.", error: {status: 500}}));
+
+        req.app.locals.___extra.databaseConnectionManager = new DatabaseConnectorManager();
+        connectionHandler( req, res );
+        expect(res.renderString).to.be.equal("error");
+        expect(JSON.stringify(res.renderObject)).to.be.equal(JSON.stringify({message: "No database connection.", error: {status: 500}}));
+    });
+});
