e72246b983feedb72682f8bb6e85c8161cf05210
David Leeper <David.Leeper@davita.com>
2017-11-07 11:23:28 -0700

    Added testing for creating, dropping, and checking existence of tables.

---
+++ test/e2e/database-connectors/database-connector-manager-test.js
@@ -0,0 +1,41 @@
+//@formatter:off
+'use strict';
+
+let chai = require( 'chai' ),
+    expect = chai.expect,
+    DatabaseConnectorManager = require('../../../database-connectors/database-connector-manager.js');
+let config = {
+    databaseConnections : [
+        {
+            name: "elasticsearch",
+            description: "Elasticsearch service.",
+            databaseConnector: "elasticsearch.js",
+            config: {
+                host: "localhost:9200",
+                log: "trace"
+            }
+        }
+    ]
+};
+
+describe( 'As a developer, I need to manage database connections.', function()
+{
+    it ( 'It should create all requested database connections', ( done ) => {
+        let dcm = new DatabaseConnectorManager();
+
+        expect(dcm).to.not.be.null;
+        console.log("=== Attempting to connect to ElasticSearch on localhost.");
+        dcm.connect(config).then ((error, param2) => {
+            expect(dcm.config).to.be.equal(config);
+            expect(dcm.databaseConnectors.length).to.be.equal(1);
+
+            let elasticSearchDC = dcm.getConnector("elasticsearch");
+            expect(elasticSearchDC).to.not.be.null;
+            dcm.disconnect().then(() => {
+                done();
+            });
+        });
+    });
+});
+
+
