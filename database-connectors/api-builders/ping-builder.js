235e5b6514b45aefa55f516ddb2077808434087e
David Leeper <David.Leeper@davita.com>
2017-11-15 13:04:06 -0700

    Added status codes to response. Tweeked database APIs, putting connection name at the beginning.

--- database-connectors/api-builders/ping-builder.js
+++ database-connectors/api-builders/ping-builder.js
@@ -9,17 +9,20 @@ function PingBuilder ( routerClass, databaseConnectionInfo ) {
         || (!req.app.locals)
         || (!req.app.locals.___extra)
         || (!req.app.locals.___extra.databaseConnectionManager)) {
+            res.status(500);
             res.render("error", {message: "No database connection manager.", error: {status: 500}});
             return;
         }
         let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
         let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
         if (!databaseConnection) {
+            res.status(500);
             res.render("error", {message: "No database connection.", error: {status: 500}});
             return;
         }
 
         databaseConnection.ping(databaseConnectionInfo).then((result) => {
+            res.status(200);
             res.send({databaseConnection: databaseConnectionInfo.name, operation: "ping", isConnected: result});
         });
     };
