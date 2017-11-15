58b1c3ef8f8cbbedc35b056d29d74341dbfd2972
David Leeper <David.Leeper@davita.com>
2017-11-15 13:14:05 -0700

    Added status codes to response. Tweeked database APIs, putting connection name at the beginning.

--- microservices/stop.js
+++ microservices/stop.js
@@ -17,12 +17,14 @@ StopService.prototype.do = function ( req, res, serviceInfo )
     return new Promise (( inResolve, inReject ) => {
         if ((!req.app) || (!req.app.locals) || (!req.app.locals.___extra) || (!req.app.locals.___extra.server)) {
             const error = { "response": "Server not found." };
+            res.status(500);
             res.send(JSON.stringify(error));
             inResolve && inResolve ( null, error );
             return;
         }
 
         const success = { "response": "Server stopping." };
+        res.status(200);
         res.send(JSON.stringify(success));
         req.app.locals.___extra.server.stop( () => { inResolve && inResolve ( null, success ) } );
     });
