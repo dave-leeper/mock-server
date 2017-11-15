f08320c3edcf98b16d8c055b09c8d189df4d5756
David Leeper <David.Leeper@davita.com>
2017-11-15 13:03:54 -0700

    Added status codes to response. Tweeked database APIs, putting connection name at the beginning.

--- database-connectors/api-builders/drop-table-builder.js
+++ database-connectors/api-builders/drop-table-builder.js
@@ -9,6 +9,7 @@ function DropTableBuilder ( routerClass, databaseConnectionInfo ) {
         || (!req.app.locals)
         || (!req.app.locals.___extra)
         || (!req.app.locals.___extra.databaseConnectionManager)) {
+            res.status(500);
             res.render("error", {message: "No database connection manager.", error: {status: 500}});
             return;
         }
@@ -16,13 +17,16 @@ function DropTableBuilder ( routerClass, databaseConnectionInfo ) {
         let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
         let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
         if (!databaseConnection) {
+            res.status(500);
             res.render("error", {message: "No database connection.", error: {status: 500}});
             return;
         }
 
         databaseConnection.dropTable( tableName ).then(( dropResult ) => {
+            res.status(200);
             res.send({ table: tableName, dropped: dropResult.acknowledged });
         }).catch( (err) => {
+            res.status(500);
             res.render("error", { message: "Error dropping " + tableName + ".", error: { status: 500, stack: err.stack }});
         });
     };
