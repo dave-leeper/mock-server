89b23f134b5c003b8201604e207d5c56521f0699
David Leeper <David.Leeper@davita.com>
2017-11-15 13:02:14 -0700

    Added status codes to response. Tweeked database APIs, putting connection name at the beginning.

--- database-connectors/api-builders/connect-builder.js
+++ database-connectors/api-builders/connect-builder.js
@@ -9,17 +9,20 @@ function ConnectBuilder ( routerClass, databaseConnectionInfo ) {
         || (!req.app.locals)
         || (!req.app.locals.___extra)
         || (!req.app.locals.___extra.databaseConnectionManager)) {
+            res.status(500);
             res.render("error", {message: "No database connection manager.", error: {status: 500}});
             return;
         }
         let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
         let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
         if (!databaseConnection) {
+            res.status(500);
             res.render("error", {message: "No database connection.", error: {status: 500}});
             return;
         }
 
         databaseConnection.connect(databaseConnectionInfo).then(() => {
+            res.status(200);
             res.send({databaseConnection: databaseConnectionInfo.name, operation: "connect", isConnected: true});
         });
     };
