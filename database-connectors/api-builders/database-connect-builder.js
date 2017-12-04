8be2bafabbffcd053fe2f9ac19183830f5411c57
David Leeper <David.Leeper@davita.com>
2017-12-04 16:04:33 -0700

    Add starting code for data handlers. Renamed some of the database handlers.

---
+++ database-connectors/api-builders/database-connect-builder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function ConnectBuilder ( routerClass, databaseConnectionInfo ) {
+    let connectHandler = (req, res) => {
+        routerClass.addHeaders(databaseConnectionInfo, res);
+
+        if ((!req)
+        || (!req.app)
+        || (!req.app.locals)
+        || (!req.app.locals.___extra)
+        || (!req.app.locals.___extra.databaseConnectionManager)) {
+            res.status(500);
+            res.render("error", {message: "No database connection manager.", error: {status: 500}});
+            return;
+        }
+        let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
+        let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
+        if (!databaseConnection) {
+            res.status(500);
+            res.render("error", {message: "No database connection.", error: {status: 500}});
+            return;
+        }
+
+        databaseConnection.connect(databaseConnectionInfo)
+            .then(() => {
+                res.status(200);
+                res.send({databaseConnection: databaseConnectionInfo.name, operation: "connect", isConnected: true});
+            });
+    };
+
+    return connectHandler;
+}
+
+module.exports = ConnectBuilder;
