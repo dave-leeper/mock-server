f474395793b3cfbfe2fb98a88447a340380e6585
David Leeper <David.Leeper@davita.com>
2017-12-04 16:05:00 -0700

    Add starting code for data handlers. Renamed some of the database handlers.

---
+++ database-connectors/api-builders/database-ping-builder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function PingBuilder ( routerClass, databaseConnectionInfo ) {
+    let pingHandler = (req, res) => {
+        routerClass.addHeaders(databaseConnectionInfo, res);
+
+        if ((!req)
+        || (!req.app)
+        || (!req.app.locals)
+        || (!req.app.locals.___extra)
+        || (!req.app.locals.___extra.databaseConnectionManager)) {
+            res.status(500);
+            res.render("error", {message: "No database connection manager.", error: {status: 500}});
+            return;
+        }
+        let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
+        let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
+        if (!databaseConnection) {
+            res.status(500);
+            res.render("error", {message: "No database connection.", error: {status: 500}});
+            return;
+        }
+
+        databaseConnection.ping(databaseConnectionInfo)
+            .then((result) => {
+                res.status(200);
+                res.send({databaseConnection: databaseConnectionInfo.name, operation: "ping", isConnected: result});
+            });
+    };
+
+    return pingHandler;
+}
+
+module.exports = PingBuilder;
