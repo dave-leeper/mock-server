7639e48da3a365f40da6c77c3c671d2c96e28ad3
David Leeper <David.Leeper@davita.com>
2017-12-04 16:05:24 -0700

    Add starting code for data handlers. Renamed some of the database handlers.

---
+++ database-connectors/api-builders/table-drop-builder.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function DropTableBuilder ( routerClass, databaseConnectionInfo ) {
+    let dropTableHandler = (req, res) => {
+        routerClass.addHeaders(databaseConnectionInfo, res);
+
+        if ((!req)
+        || (!req.app)
+        || (!req.app.locals)
+        || (!req.app.locals.___extra)
+        || (!req.app.locals.___extra.databaseConnectionManager)) {
+            res.status(500);
+            res.render("error", {message: "No database connection manager.", error: {status: 500}});
+            return;
+        }
+        let tableName = req.params.name;
+        let databaseConnectionManager = req.app.locals.___extra.databaseConnectionManager;
+        let databaseConnection = databaseConnectionManager.getConnector(databaseConnectionInfo.name);
+        if (!databaseConnection) {
+            res.status(500);
+            res.render("error", {message: "No database connection.", error: {status: 500}});
+            return;
+        }
+
+        databaseConnection.dropTable( tableName )
+            .then(( dropResult ) => {
+                res.status(200);
+                res.send({ table: tableName, dropped: dropResult.acknowledged });
+            }).catch((err) => {
+                res.status(500);
+                res.render("error", { message: "Error dropping " + tableName + ".", error: { status: 500, stack: err.stack }});
+            });
+    };
+
+    return dropTableHandler;
+}
+
+module.exports = DropTableBuilder;
