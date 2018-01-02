15ff417bfc24c3ee99ec798baf6f98aa28631aeb
David Leeper <David.Leeper@davita.com>
2018-01-02 10:46:58 -0700

    Update working.

--- database-connectors/api-builders/data-update-builder.js
+++ database-connectors/api-builders/data-update-builder.js
@@ -35,43 +35,44 @@ function DataUpdateBuilder( routerClass, databaseConnectionInfo )
                 routerClass.sendErrorResponse(error, res);
                 return;
             }
-            if (req.param('index')) {
+            if (req.param.index) {
                 const error = { message: "Error, no index name provided.", error: { status: 500 }};
                 routerClass.sendErrorResponse(error, res);
                 return;
             }
-            if (req.param('type')) {
+            if (req.param.type) {
                 const error = { message: "Error, no data type provided.", error: { status: 500 }};
                 routerClass.sendErrorResponse(error, res);
                 return;
             }
-            if (req.param('id')) {
+            if (req.param.id) {
                 const error = { message: "Error, no record id provided.", error: { status: 500 }};
                 routerClass.sendErrorResponse(error, res);
                 return;
             }
-            let updateData = JSON.parse(req.files.fileUploaded.data.toString());
+            let updateDoc = JSON.parse(req.files.fileUploaded.data.toString());
             let index = req.params.index;
             let type = req.params.type;
             let id = req.params.id;
-
-            databaseConnection.update({
+            let data = {
                 index: index,
                 type: type,
                 id: id,
-                body: updateData
-            }, (error, response) => {
+                body: { doc: updateDoc }
+            };
+
+            databaseConnection.update(data, (error, response) => {
                 if (error) {
                     const error = { message: "Error updating record (id: " + id + "). " + err.error, error: { status: 500 }};
                     routerClass.sendErrorResponse(error, res);
                     return;
                 }
-                const success = {status: "success", operation: "Insert data to " + index};
+                const success = {status: "success", operation: "Update data for " + index};
                 res.status(200);
                 res.send(JSON.stringify(success));
             });
         } catch (err) {
-            const error = { message: "Error inserting ElasticSearch data.", error: { status: 500, stack: err.stack }};
+            const error = { message: "Error updating ElasticSearch data.", error: { status: 500, stack: err.stack }};
             routerClass.sendErrorResponse(error, res);
         }
     };
