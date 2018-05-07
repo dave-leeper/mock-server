e70f92cf277df30677245f090cf7e09652e68007
David Leeper <dleeper@parler.com>
2018-05-07 09:57:58 -0700

    Got logging working. Added default logging config. Added body-to-log microservice.

--- microservices/body-to-log.js
+++ microservices/body-to-log.js
@@ -1,12 +1,11 @@
 'use strict';
 
-const path = require('path');
+const logger = require('../util/logger-utilities');
 
-const FILE_PATH = path.join(__dirname , "/../files/");
 /**
  * @constructor
  */
-function BodyToConsoleService ( )
+function BodyToLogService ( )
 {
 }
 
@@ -15,16 +14,32 @@ function BodyToConsoleService ( )
  * @param res {Object} - The response object.
  * @param serviceInfo - Service config info.
  */
-BodyToConsoleService.prototype.do = function ( req, res, serviceInfo )
+BodyToLogService.prototype.do = function (req, res, serviceInfo )
 {
     return new Promise (( inResolve, inReject ) => {
+        let level = "DEBUG";
+        if (serviceInfo && serviceInfo.serviceData && serviceInfo.serviceData.level) {
+            level = serviceInfo.serviceData.level.toUpperCase();
+        }
+        if ( "ALL" === level ) logger.all(req.body);
+        else if ( "TRACE" === level ) logger.trace(req.body);
+        else if ( "DEBUG" === level ) logger.debug(req.body);
+        else if ( "INFO" === level ) logger.info(req.body);
+        else if ( "WARN" === level ) logger.warn(req.body);
+        else if ( "ERROR" === level ) logger.error(req.body);
+        else if ( "FATAL" === level ) logger.fatal(req.body);
+        else {
+            const error = { status: "error", operation: "Invalid logging level supplied in service information." };
+            res.status(400);
+            res.send(JSON.stringify(error));
+            inReject && inReject(error, null);
+            return;
+        }
         const success = { status: "success", operation: "Print body to console" };
-        console.log(req.body);
         res.status(200);
         res.send(JSON.stringify(success));
-
         inResolve && inResolve(null, success);
     });
 };
 
-module.exports = BodyToConsoleService;
+module.exports = BodyToLogService;
