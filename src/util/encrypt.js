aaf1f1c9f4e5c37f01206bb656e9a28a681c55c9
dave-leeper <magicjtv@gmail.com>
2019-11-19 05:54:36 -0700

    Added multile groups, headers, and cookies to Add User functionality. Added missing destionaion error message

---
+++ src/util/encrypt.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+
+function Encrypt(){};
+Encrypt.key = crypto.randomBytes(32);
+Encrypt.iv = crypto.randomBytes(16);
+
+Encrypt.encrypt = function (text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(Encrypt.key), Encrypt.iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: Encrypt.iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+Encrypt.decrypt = function (text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(Encrypt.key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+module.exports = Encrypt;
